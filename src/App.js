1f0a3a3895b39688e393f7709977a951c4c98b0a
MarkoTeshomeTECH <zeanbesa7066@gmail.com>
2024-09-23 14:04:49 +0300

    pizza delivery intial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Pizza />
+    </div>
+  );
+}
+function Pizza() {
+  return <h1>Pizza</h1>;
+}
